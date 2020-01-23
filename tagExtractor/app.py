84eba69459313a36cbe5322f2dc9e34699f43111
vijay <vijay.poudel1@gmail.com>
2020-01-23 20:37:18 +0530

    Tag Extractor API changes

--- tagExtractor/app.py
+++ tagExtractor/app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 
 @app.route('/getTags/')
-def getTags():
-    return service.TagExtractor().extractTag()
+def get_tags():
+    return service.TagExtractor().extractTag("")
+
+
+@app.route('/getTags/<brand>')
+def get_tags_for_brand(brand):
+    return service.TagExtractor().extractTag(brand)
 
 
 if __name__ == "__main__":
--- tagExtractor/extractorService.py
+++ tagExtractor/extractorService.py
@@ -1,11 +1,23 @@
 import os,json,config
 import numpy as np
 import nltk as nltk
-nltk.download('stopwords')
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 
 
 class TagExtractor:
+
+    def remove_verb_adverb_adjective(self, words):
+        verb_container = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'RB', 'RBR', 'RBS', 'RP', 'JJ', 'JJR', 'JJS', 'DT','MD']
+        tagged_words = nltk.pos_tag(words)
+        print(tagged_words)
+        words_list = [w[0] for w in tagged_words if not w[1] in verb_container]
+        print(words_list)
+        return words_list
 
     def removePunctuation(self, words):
         symbols = "!\"#$%&()*+./:;<=>?@[\]^_`{|}~\n"
@@ -31,12 +43,14 @@ class TagExtractor:
                 keywords.append(i)
         return keywords
 
-    def removeSingleCharacterWords(self, words):
+    def lemmatization(self, words):
         keywords = []
+        lemmatizer = WordNetLemmatizer()
         for i in words:
             if len(i) > 3:
-                keywords.append(i)
+                keywords.append(lemmatizer.lemmatize(i))
         return keywords
+
 
     def removeStopWords(self, words):
         keywords = []
@@ -48,39 +62,41 @@ class TagExtractor:
                 keywords.append(i)
         return keywords
 
-    def getTheRelevantWords(self, words, documents, nameOfRetailer):
+    def get_the_relevant_words(self, words, documents, nameOfRetailer):
         mapOfWords = dict.fromkeys(words, 0)
         wordsfinal = set(words)
         for i in wordsfinal:
             for j in documents:
                 if i in j:
                     mapOfWords[i] += 1
-        processeddict = {k: v for k, v in sorted(mapOfWords.items(), key=lambda item: item[1], reverse=True)}
-        return [*processeddict]
+        processed_dict = {k: v for k, v in sorted(mapOfWords.items(), key=lambda item: item[1], reverse=True)}
+        return [*processed_dict]
 
-    def extractTag(self):
+    def extractTag(self, brand):
         path = config.FILE_LOCATION
         tagsForAllRetailers = {}
         whereToPickFrom = {'wikipedia': 'paragraphdump', 'crunchbase': 'longdescription', 'yahoo': 'paragraphdump'}
         jsonDocuments = [jsonFile for jsonFile in os.listdir(path)]
         for json_file in jsonDocuments:
-            json_file_path = os.path.join(path, json_file)
-            with open(json_file_path, "r") as f:
-                j = json.load(f)
-                arrayOfDocuments = []
-                allTheWords = []
-                for x, z in whereToPickFrom.items():
-                    if x not in j or z not in j[x]:
-                        continue
-                    refinedText = (str)(self.removePunctuation(j[x][z]))
-                    arrayOfDocuments.append(refinedText.lower())
-                    allTheWords += set(refinedText.split(' '))
-            bagOfTags = self.removeNonString(allTheWords)
-            bagOfTags = self.removePunctuation(bagOfTags)
-            bagOfTags = self.makeAllWordsSmallCaps(bagOfTags)
-            bagOfTags = self.removeStopWords(bagOfTags)
-            bagOfTags = self.removeSingleCharacterWords(bagOfTags)
-            bagOfTags = self.removeRetailerName(bagOfTags, json_file.split('.')[0])
-            relevantTags = self.getTheRelevantWords(bagOfTags, arrayOfDocuments, json_file.split('.')[0])
-            tagsForAllRetailers[json_file.split('.')[0]] = relevantTags[0:25]
+            if brand.lower() in json_file.lower():
+                json_file_path = os.path.join(path, json_file)
+                with open(json_file_path, "r") as f:
+                    j = json.load(f)
+                    arrayOfDocuments = []
+                    allTheWords = []
+                    for x, z in whereToPickFrom.items():
+                        if x not in j or z not in j[x]:
+                            continue
+                        refinedText = (str)(self.removePunctuation(j[x][z]))
+                        arrayOfDocuments.append(refinedText.lower())
+                        allTheWords += nltk.word_tokenize(refinedText)
+                bagOfTags = self.removeNonString(allTheWords)
+                bagOfTags = self.removeStopWords(bagOfTags)
+                bagOfTags = self.remove_verb_adverb_adjective(bagOfTags)
+                bagOfTags = self.removePunctuation(bagOfTags)
+                bagOfTags = self.makeAllWordsSmallCaps(bagOfTags)
+                bagOfTags = self.lemmatization(bagOfTags)
+                bagOfTags = self.removeRetailerName(bagOfTags, json_file.split('.')[0])
+                relevantTags = self.get_the_relevant_words(bagOfTags, arrayOfDocuments, json_file.split('.')[0])
+                tagsForAllRetailers[json_file.split('.')[0]] = relevantTags[0:21]
         return json.dumps(tagsForAllRetailers)
