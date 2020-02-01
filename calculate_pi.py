0f417fe587de380ff731738286cca25b7cfb5b29
Saurav <spike.samantray@gmail.com>
2020-02-01 13:21:40 +0530

    adding demo calculate_pi.py file for azure livy submission

---
+++ calculate_pi.py
@@ -0,0 +1,15 @@
+import sys
+from random import random
+from operator import add
+
+# Change this to play around with larger sets
+partitions = 2
+n = 10000 * partitions
+
+def f(_):
+    x = random() * 2 - 1
+    y = random() * 2 - 1
+    return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+print("Pi is roughly %f" % (4.0 * count / n))
